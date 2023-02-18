07d640ca631c3d3af6981354f80a66982a17cede
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2023-02-18 13:21:49 +0500

    title and logo changed

--- src/Components/Partials/Navbar.jsx
+++ src/Components/Partials/Navbar.jsx
@@ -7,7 +7,8 @@ function Navbar(props) {
   return (
     <div className="sticky-top">
       <nav className="pt-3 navbar navbar-expand-lg navbar-light background-dark paddingX">
-        <img src={logo} alt="" />
+        {/* <img src={logo} alt="" /> */}
+        <h1 className="font-clash text-white">Digi-Corn</h1>
         <button
           className="navbar-toggler"
           type="button"
